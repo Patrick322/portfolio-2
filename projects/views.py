54ab294ac3e93d41f38bc28f6cb179e663e8ff5c
Patrick595 <patrickbuong@gmail.com>
2020-02-27 11:59:43 +0300

    added child class templates

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,28 @@
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to my portfolio application!Have fun')
+    return render(request, 'welcome.html')
+
+def projects_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-projects/today-projects.html',{'date': date})
+
+
+
+def past_days_projects(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(projects_of_day)
+
+    return render(request, 'all-projects/past-projects.html', {"date":date})
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/portfolio-2/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
